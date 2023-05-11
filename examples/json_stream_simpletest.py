85c248d8e972b81f7a9f42a3dcf0210691e384cb
Scott Shawcroft <scott@tannewt.org>
2023-05-10 17:03:28 -0700

    Add MLB example

--- examples/json_stream_simpletest.py
+++ examples/json_stream_simpletest.py
@@ -2,3 +2,26 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 Scott Shawcroft for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
+
+import adafruit_requests
+import adafruit_json_stream as json_stream
+import socketpool
+import ssl
+import wifi
+import time
+
+pool = socketpool.SocketPool(wifi.radio)
+session = adafruit_requests.Session(pool, ssl.create_default_context())
+
+SCORE_URL = f"http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
+
+while True:
+    resp = session.get(SCORE_URL)
+    json_data = json_stream.load(resp.iter_content(32))
+    for event in json_data["events"]:
+        if "Seattle" not in event["name"]:
+            continue
+        for competition in event["competitions"]:
+            for competitor in competition["competitors"]:
+                print(competitor["team"]["displayName"], competitor["score"])
+    time.sleep(60)
